f8be3ab60b3153eedbcd5fdb5650aa923879a38a
Jatinder <Jatinder.kumar@qualhon.com>
2024-07-24 19:27:51 +0530

    first push

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by Kaushal on 24/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Kaushal on 24/07/24.
+//
+
+import Foundation
+
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
